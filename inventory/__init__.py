c176d8680ed3367a7d9694aca2047d25ee978d8c
ramiv212 <ramiv212@hotmail.com>
2022-05-06 10:50:50 -0500

    added database backup download and upload

--- inventory/__init__.py
+++ inventory/__init__.py
@@ -4,10 +4,15 @@ from flask_wtf.csrf import CSRFProtect
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+
+UPLOAD_FOLDER = f"{os.getcwd()}/inventory/uploads"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getcwd()}/inventory/uploads/inventory.db'
 app.config['SECRET_KEY'] = '1c3c7811fb09da0e1cadcb7e'
 app.config['SESSION_COOKIE_SECURE'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
@@ -19,11 +24,13 @@ from .home_page import homepage
 from .events_page import eventspage
 from .json_routes import jsonroutes
 from .resources import json_routes
+from .admin import adminpage
 
 app.register_blueprint(homepage, url_prefix='/')
 app.register_blueprint(eventspage, url_prefix='/events/')
 app.register_blueprint(jsonroutes, url_prefix='/json-routes/')
 app.register_blueprint(json_routes, url_prefix='/json/')
+app.register_blueprint(adminpage, url_prefix='/admin/')
 
 
 print("Started!!")
---
+++ inventory/admin.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,request,send_file,Blueprint
+from werkzeug.utils import secure_filename
+import os
+
+adminpage = Blueprint('admin',__name__)
+
+@adminpage.route("/", methods=['GET'])
+def admin():
+	return render_template('admin.html')
+
+@adminpage.route("/download-db", methods=['GET', 'POST'])
+def download_db():
+	return send_file(f'{os.getcwd()}/inventory/uploads/inventory.db', as_attachment=True)
+
+@adminpage.route("/upload-db", methods=['GET', 'POST'])
+def upload_db():
+   if request.method == 'POST': # check if the method is post
+      f = request.files['file'] # get the file from the files object
+      # Saving the file in the required destination
+      f.save(os.path.join(f'{os.getcwd()}/inventory/uploads',secure_filename(f.filename))) # this will secure the file
+
+      print(os.path.join(f'{os.getcwd()}/inventory/uploads',secure_filename(f.filename)))
+
+      return 'file uploaded successfully' # Display thsi message after uploading
+		
+if __name__ == '__main__':
+   app.run() # running the flask app
