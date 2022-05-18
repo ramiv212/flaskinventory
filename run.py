b8035cb2fb338a1decf67fb2f75cfca1ac0e2f72
ramiv212 <ramiv212@hotmail.com>
2022-05-18 11:37:14 -0500

    testing scanner

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from inventory import app
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0")
